6c5f8c6e841a7c8b4bb3d65d5b320f1ac50f10e2
Leslie Lauw <32265116+LALauw@users.noreply.github.com>
2025-02-05 12:05:11 +0100

    update config

--- ewhs/config.go
+++ ewhs/config.go
@@ -6,20 +6,14 @@ type Config struct {
 	WmsCode      string
 	CustomerCode string
 	Testing      bool
-	Domain 		 string
-	App			 string
-	AppVersion	 string
 }
 
-func NewConfig(username string, password string, wmsCode string, customerCode string, testing bool, domain string, app string, appVersion string) *Config {
+func NewConfig(username string, password string, wmsCode string, customerCode string, testing bool) *Config {
 	return &Config{
 		Username:     username,
 		Password:     password,
 		WmsCode:      wmsCode,
 		CustomerCode: customerCode,
 		Testing:      testing,
-		Domain:	      domain,
-		App:		  app,
-		AppVersion:   appVersion,
 	}
 }
